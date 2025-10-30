d68172745d9ed865b99e1ae026178b4a676dbcd6
luigi palladino <luigi.palladino160306@gmail.com>
2025-10-30 15:35:01 +0100

    init

---
+++ NoleggioVeicoliNew/services/Noleggio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoleggioVeicoliNew.services
+{
+    public class Noleggio
+    {
+        // public Cliente cliente;
+        // public Veicolo veicolo;
+        public DateTime DataInizio { get; }
+        public int DurataGiorni
+        
+    }
+}
