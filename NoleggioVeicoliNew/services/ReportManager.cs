d81501ed07fbb07e5b25646c0fe918f6cae16a24
Sandro Vecchiarelli <sandro.vecchiarelli@pomiager.com>
2025-10-31 16:16:57 +0100

    last Version

--- NoleggioVeicoliNew/services/ReportManager.cs
+++ NoleggioVeicoliNew/services/ReportManager.cs
@@ -19,7 +19,7 @@ namespace NoleggioVeicoliNew.services
             List<Veicolo> veicoliNonNoleggiati = db.GetAllVeicoli().Where(v => !v.Noleggiato).ToList();
             //int vDisponibili= allNoleggi.Where(n => !n.Veicolo.Noleggiato && data.Date == DateTime.Today).ToList().Count();
             //int vDisponibili= db.GetAllVeicoli().Count() - veicoliNonNoleggiati.Count();
-
+            //
             Console.WriteLine($"===Report Giornaliero===\r\nTotale incasso: {totale} Euro\r\nMedia Giorni di Noleggio: {mediaGiorniNoleggio}\r\nVeicoli Disponibili: {veicoliNonNoleggiati.Count}");
         }
 
