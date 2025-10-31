c6f86bf83b457560c7554e97776c3b8dafd6f66a
luigi palladino <luigi.palladino160306@gmail.com>
2025-10-31 11:27:25 +0100

    Implementazioni varie

---
+++ NoleggioVeicoliNew/models/Veicolo.cs
@@ -0,0 +1,31 @@
+﻿using NoleggioVeicoliNew.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoleggioVeicoliNew.models
+{
+    public delegate void VeicoloNoleggiatoEventHandler(NoleggioManager sen, Veicolo veicolo);
+    public class Veicolo
+    {
+
+        public event VeicoloNoleggiatoEventHandler NoleggioIniziato;
+
+        public Veicolo(NoleggioManager nl)
+        {
+            nl.Subscribe(this);
+        }
+
+        public void Noleggiato()
+        {
+            Console.WriteLine("sucaaaaaaa");
+        }
+
+        private void OnVeicoloNoleggiato()
+        {
+
+        }
+    }
+}
---
+++ NoleggioVeicoliNew/services/NoleggioManager.cs
@@ -0,0 +1,18 @@
+﻿using NoleggioVeicoliNew.interfaces;
+using NoleggioVeicoliNew.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoleggioVeicoliNew.services
+{
+    public delegate void OnVeicoloNoleggiatoEventHandler(NoleggioManager sen,Noleggio noleggio);
+    public delegate void OnVeicoloRestituitoEventHandler(NoleggioManager sendr, Noleggio Noleggioargs);
+    public class NoleggioManager
+    {
+
+    }
+}
