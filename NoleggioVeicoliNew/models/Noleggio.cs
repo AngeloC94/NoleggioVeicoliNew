1289aed7859efb238e37cebb940e752470e69396
Gabriele Vecchi <vecchigabriele03@gmail.com>
2025-10-31 11:38:44 +0100

    commit - cliente e sottoclassi veicolo

--- NoleggioVeicoliNew/models/Noleggio.cs
+++ NoleggioVeicoliNew/models/Noleggio.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NoleggioVeicoliNew.services
+namespace NoleggioVeicoliNew.models
 {
     public class Noleggio
     {
